e4321faa4f658f0e7f0afc90776f2ebfb39b6681
parkwoobin <oi3oi3oi@naver.com>
2026-03-05 13:27:31 +0900

    레시피 카드 관리 기능 구현 및 README 수정

--- ColorRGB/Program.cs
+++ ColorRGB/Program.cs
@@ -1,4 +1,39 @@
 using System;
 
 // README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+
+ColorRGB red = new ColorRGB(255, 0, 0);
+ColorRGB white = new ColorRGB(255, 255, 255);
+ColorRGB custom = new ColorRGB(100, 150, 200);
+
+Console.WriteLine("=== RGB 색상 밝기 계산===");
+
+Console.Write($"빨강 - R: {red.R}, G: {red.G}, B: {red.B} → ");
+red.GetBrightness();
+Console.Write($"흰색 - R: {white.R}, G: {white.G}, B: {white.B} → ");
+white.GetBrightness();
+Console.Write($"커스텀 - R: {custom.R}, G: {custom.G}, B: {custom.B} → ");
+custom.GetBrightness();
+
+
+struct ColorRGB
+{
+    public int R;
+    public int G;
+    public int B;
+
+
+    public ColorRGB(int v1, int v2, int v3)
+    {
+        this.R = v1;
+        this.G = v2;
+        this.B = v3;
+    }
+
+    public void GetBrightness()
+    {
+        int brightness = (R + G + B) / 3;
+        Console.WriteLine($"밝기: {brightness}");
+    }
+
+}
--- RecipeCard/Program.cs
+++ RecipeCard/Program.cs
@@ -1,4 +1,76 @@
 using System;
 
 // README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+
+Ingredient[] ingredients = new Ingredient[]
+{
+    new Ingredient("스파게티면", 200, "g"),
+    new Ingredient("토마토소스", 150, "ml"),
+    new Ingredient("양파", 1, "개"),
+    new Ingredient("마늘", 3, "쪽")
+};
+
+RecipeCard recipe = new RecipeCard("토마토 파스타", 2, ingredients);
+Console.WriteLine("=== 원본 레시피 ===");
+recipe.PrintRecipe();
+
+Console.WriteLine("\n=== 4인분 레시피 ===");
+recipe.ScaleRecipe(4);
+
+
+
+
+
+
+
+struct Ingredient
+{
+    public string Name;
+    public double Amount;
+    public string Unit;
+
+    public Ingredient(string name, double amount, string unit)
+    {
+        this.Name = name;
+        this.Amount = amount;
+        this.Unit = unit;
+    }
+}
+
+struct RecipeCard
+{
+    public string Name;
+    public int Servings;
+    public Ingredient[] Ingredients;
+
+    public RecipeCard(string name, int servings, Ingredient[] ingredients)
+    {
+        this.Name = name;
+        this.Servings = servings;
+        this.Ingredients = ingredients;
+    }
+
+    public void PrintRecipe()
+    {
+        Console.WriteLine($"[{Name}] ({Servings}인분)");
+        Console.WriteLine("재료:");
+        foreach (var ingredient in Ingredients)
+        {
+            Console.WriteLine($"- {ingredient.Name}: {1 * ingredient.Amount}{ingredient.Unit}");
+        }
+    }
+
+    public void ScaleRecipe(int newServings)
+    {
+        double change = (double)newServings / Servings;
+        Console.WriteLine($"[{Name}] ({newServings}인분)");
+        Console.WriteLine("재료:");
+        for (int i = 0; i < Ingredients.Length; i++)
+        {
+            Ingredients[i].Amount *= change;
+            Console.WriteLine($"- {Ingredients[i].Name}: {Ingredients[i].Amount}{Ingredients[i].Unit}");
+        }
+        Servings = newServings;
+    }
+
+}
